72708779ee36ed7e16c95ac73c310b6177644b9e
Sifou Biri <sifoubiri@gmail.com>
2021-03-31 13:14:48 -0700

    init

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import "../global.css";
+</script>
+
+<nav class="flex flex-row p-10">
+    <a class="font-bold">Knowbie</a>
+    <div class="flex-grow h-8"></div>
+    <button class="justify-self-end self-end uppercase font-bold text-xs red-text">Contact us</button>
+</nav>
+
+<main>
+    <slot />
+</main>
+
