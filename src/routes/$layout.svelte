d261dbb03f21901f1d6fa854fc7ddb5d334220b0
Sifou Biri <sifoubiri@gmail.com>
2021-03-31 20:47:49 -0700

    update

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -5,7 +5,7 @@
 <nav class="flex flex-row p-10">
     <a class="font-bold">Knowbie</a>
     <div class="flex-grow"></div>
-    <a class="justify-self-end self-end uppercase font-bold text-xs red-text">Contact us</a>
+    <button class="justify-self-end self-end uppercase font-bold text-xs red-text">Contact us</button>
 </nav>
 
 <main>
