98dbf06eb03ad9a41a5b72a035e5378067bf42f8
Dmitrii <31366100+Kanadets@users.noreply.github.com>
2021-05-27 17:45:58 -0700

    Header, about, for restaurants, and some no name sections

--- deploy-web.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pnpm build
-rm -rf ~/Projects/Knowbie/Backend/Realm/website/hosting/files/*
-cp -rf  build/*  ~/Projects/Knowbie/Backend/Realm/website/hosting/files
-cd ~/Projects/Knowbie/Backend/Realm/website/ && realm-cli push --remote="website-tkniu" --include-hosting
--- src/lib/stores.js
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-import * as Realm from "realm-web";
-
-export const app = writable(new Realm.App({id: "kwebsite-dfvrm"}));
